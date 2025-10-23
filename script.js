af2abb2e0f46926fab08294b92e6787a15580210
rahmadjabon-gif <rahmadjabon@gmail.com>
2025-10-23 10:10:45 +0700

    Add click event to Instagram button

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1. pilih tombol berdasarkan id-nya
+const tombolKunjungiInstagramSaya = document.getElementById('KunjungiInstagramSaya');
+tombolKunjungiInstagramsaya.addEvenListener('click',function(){
+  alert('Halo! terimakasih dan sampai jumpa!');
+});
